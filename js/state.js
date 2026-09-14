52cad33d8f614fe979249018417d439ea19d63b1
pritishnandy060307-jpg <pritishnandy060307@gmail.com>
2026-09-14 17:58:55 +0530

    Create central race state module

---
+++ js/state.js
@@ -0,0 +1,13 @@
+export const raceState = {
+    sessionActive: false,
+    sessionSeconds: 0,
+    lapNumber: 1,
+    bestLap: null,
+    telemetry: {
+        speed: 0,
+        rpm: 0,
+        gear: 1,
+        throttle: 0,
+        brake: 0
+    }
+};
