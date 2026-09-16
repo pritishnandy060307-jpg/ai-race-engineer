46285fb465ba5f603ecf9a39aa5184ef144d4a0c
pritishnandy060307-jpg <pritishnandy060307@gmail.com>
2026-09-17 05:19:35 +0530

    Load driver performance analysis

--- js/vehicle-navigation.js
+++ js/vehicle-navigation.js
@@ -5,6 +5,7 @@ import "./fuel-consumption.js?v=1";
 import "./telemetry-charts.js?v=1";
 import "./race-insights.js?v=1";
 import "./phase2-dashboard.js?v=1";
+import "./performance-analysis.js?v=1";
 
 function moveSessionControls() {
     const status = document.querySelector(".status");
